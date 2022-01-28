a33407314af806410b57d1b7f4ac896c66ba4dc4
Lars <l.gessner@posteo.de>
2022-01-28 11:03:40 +0100

    fix lst items to string

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const title = document.createElement("h1")
 const inputField = document.createElement("input")
 inputField.placeholder = "type your 🚀"
 
-const lst = [🏆, 👻, 🤖]
+const lst = ["🏆","👻", "🤖"]
 
 const inject = () => {
     
